c7f59ec8d6588d7d6b1f83cb3a58f9179881e083
GALTdea <gustavovalenzuela1180@gmail.com>
2019-12-09 15:22:45 -0800

    created challenge object

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_224927) do
+ActiveRecord::Schema.define(version: 2019_12_09_231628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "challenges", force: :cascade do |t|
+    t.string "title"
+    t.boolean "completed"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_challenges_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +30,5 @@ ActiveRecord::Schema.define(version: 2019_12_09_224927) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "challenges", "users"
 end
